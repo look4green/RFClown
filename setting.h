e4f01a41f56c58d9cc14e345dc71c76c05f1b2d5
look4green <look4green@users.noreply.github.com>
2025-12-16 17:03:13 +0800

    Add channel array size declarations in setting.h
    Added size declarations for various channel arrays to support dynamic channel management.

--- setting.h
+++ setting.h
@@ -2,9 +2,11 @@
    This software is licensed under the MIT License:
    https://github.com/cifertech/rfclown
    ________________________________________ */
-   
+
 #ifndef setting_H
 #define setting_H
+
+#pragma once
 
 // --- 1. Library Includes (All required for different modules) ---
 #include <BLEDevice.h>
@@ -16,6 +18,8 @@
 #include <string>
 #include <SPI.h>
 #include <Arduino.h> // Ensure Arduino core types are available
+#include "config.h"
+
 
 // --- 2. NeoPixel Function Prototypes (Defined in neopixell.cpp) ---
 void neopixelSetup();
@@ -43,9 +47,35 @@ extern volatile bool ChangeRequested2;
 extern unsigned long lastPressTime;
 extern const unsigned long debounceDelay; // Assuming debounceDelay is defined in config.h
 
+
+// --- Channel Array Declarations ---
 extern byte channelGroup_1[];
 extern byte channelGroup_2[];
 extern byte channelGroup_3[];
+
+extern const byte bluetooth_channels[];
+extern const byte ble_channels[];
+extern const byte WiFi_channels[];
+extern const byte usbWireless_channels[];
+extern const byte videoTransmitter_channels[];
+extern const byte rc_channels[];
+extern const byte zigbee_channels[];
+extern const byte nrf24_channels[];
+
+// --- NEW: Channel Array Size Declarations (Used by RfClown.ino) ---
+extern const size_t CHANNEL_GROUP_1_SIZE;
+extern const size_t CHANNEL_GROUP_2_SIZE;
+extern const size_t CHANNEL_GROUP_3_SIZE;
+
+extern const size_t BLUETOOTH_CHANNELS_SIZE;
+extern const size_t BLE_CHANNELS_SIZE;
+extern const size_t WIFI_CHANNELS_SIZE;
+extern const size_t USB_WIRELESS_CHANNELS_SIZE;
+extern const size_t VIDEO_TRANSMITTER_CHANNELS_SIZE;
+extern const size_t RC_CHANNELS_SIZE;
+extern const size_t ZIGBEE_CHANNELS_SIZE;
+extern const size_t NRF24_CHANNELS_SIZE;
+
 
 // --- 4. Radio/Utility Function Prototypes (Defined in settings.cpp) ---
 void configureNrf(RF24 &radio);
@@ -62,7 +92,8 @@ void conf();
 extern const uint8_t txt_n[];
 extern const uint8_t txt_c[];
 extern const uint8_t txt_v[];
-extern const unsigned char cred [] PROGMEM;
+extern const unsigned char cred [];
+//extern const unsigned char cred [] PROGMEM;
 
 // --- 6. External Declarations for Channel Arrays (Defined ONCE in settings.cpp) ---
 extern const byte bluetooth_channels[];
@@ -73,5 +104,13 @@ extern const byte videoTransmitter_channels[];
 extern const byte rc_channels[];
 extern const byte zigbee_channels[];
 extern const byte nrf24_channels[];
+
+// --- Array Size Declarations (New) ---
+extern const size_t CHANNEL_GROUP_1_SIZE;
+extern const size_t CHANNEL_GROUP_2_SIZE;
+extern const size_t CHANNEL_GROUP_3_SIZE;
+extern const size_t BLUETOOTH_CHANNELS_SIZE;
+extern const size_t BLE_CHANNELS_SIZE;
+// ... (Add extern const size_t for all your other channel arrays) ...
 
 #endif
