0af2b1c5cf9bf4eac01e6b62737f22ea67756a35
look4green <look4green@users.noreply.github.com>
2025-12-18 14:19:02 +0800

    Revise license and enhance config.h with externs
    Updated license information and added external declarations for various variables and channels.

--- config.h
+++ config.h
@@ -1,42 +1,67 @@
 /* ____________________________
-   This software is licensed under the MIT License:
-   https://github.com/cifertech/rfclown
-   ________________________________________ */
+   This software is licensed under the MIT License:
+   https://github.com/look4green/RFClown
+   ________________________________________ */
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// --- Hardware and Screen Size ---
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Adafruit_NeoPixel.h>
+#include <RF24.h>
+
 #define SCREEN_W 128
 #define SCREEN_H 64
 
-// --- Button Pins ---
-#define PIN_BTN_L   27
-#define PIN_BTN_R   25
-#define PIN_BTN_S   26
-//#define debounceDelay 200 // Add debounce delay here
+// ---------------- Pins ----------------
+#define PIN_BTN_L  27
+#define PIN_BTN_R  25
+#define PIN_BTN_S  26
 
-// --- nRF24-specific Pins ---
-#define NRF_CE_PIN_A    5    
+#define NRF_CE_PIN_A    5   
 #define NRF_CSN_PIN_A   17 
 #define NRF_CE_PIN_B    16  
 #define NRF_CSN_PIN_B   4  
 #define NRF_CE_PIN_C    15  
 #define NRF_CSN_PIN_C   2  
 
-// --- U8G2 Font Definitions ---
-// NOTE: These should ideally be defined in the .cpp file where they are used, 
-// but can remain here for consistency if they are only used as const pointers.
-#define FONT_SMALL        u8g2_font_5x8_tf
-#define FONT_MEDIUM       u8g2_font_6x12_tf
-#define FONT_ICON_FALLBACK u8g2_font_open_iconic_thing_2x_t
+// --- External Declarations (Tell compiler they exist elsewhere) ---
+extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+extern Adafruit_NeoPixel pixels;
+extern RF24 RadioA;
+extern RF24 RadioB;
+extern RF24 RadioC;
 
+// Use 'extern' for variables to avoid multiple definitions
+enum OperationMode {WiFi_MODULE, VIDEO_TX_MODULE, RC_MODULE, BLE_MODULE, Bluetooth_MODULE, USB_WIRELESS_MODULE, ZIGBEE_MODULE, NRF24_MODULE};
+extern OperationMode current_Mode;
 
-// --- ENUMS (Safe to repeat) ---
-enum OperationMode {
-    WiFi_MODULE, VIDEO_TRANSMITTER_MODULE, RC_MODULE, BLE_MODULE, 
-    Bluetooth_MODULE, USB_WIRELESS_MODULE, ZIGBEE_MODULE, NRF24_MODULE
-};
 enum Operation {DEACTIVE_MODE, ACTIVE_MODE};
+extern volatile Operation current;
 
-#endif // CONFIG_H
+extern byte channelGroup_1[];
+extern byte channelGroup_2[];
+extern byte channelGroup_3[];
+
+extern const byte bluetooth_channels[];
+extern const byte ble_channels[];
+extern const byte WiFi_channels[];
+extern const byte usbWireless_channels[];
+extern const byte videoTransmitter_channels[];
+extern const byte rc_channels[];
+extern const byte zigbee_channels[];
+extern const byte nrf24_channels[];
+
+extern volatile bool ChangeRequested;
+extern volatile bool ChangeRequested1;
+extern volatile bool ChangeRequested2;
+
+extern unsigned long lastPressTime;
+const unsigned long debounceDelay = 200; // Const is okay in headers
+
+// Fonts (Keep as static const)
+static const uint8_t* FONT_SMALL  = u8g2_font_5x8_tf;
+static const uint8_t* FONT_MEDIUM = u8g2_font_6x12_tf;
+
+#endif
